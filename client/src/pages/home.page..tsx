df171d70e001912dfe63d577920775f25c89c763
Tigo <rf.tigas@gmail.com>
2025-03-25 20:00:25 -0300

    client: add home page

---
+++ client/src/pages/home.page..tsx
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <section id="home">
+      <div className="flex flex-col justify-center h-screen items-center bg-zinc-800">
+        <h1 className="text-zinc-50 text-4xl">Welcome to Snippout</h1>
+      </div>
+    </section>
+  )
+}
+
+export default HomePage
