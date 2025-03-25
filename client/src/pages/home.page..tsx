32fbff6b0fea20abd17fb66c44e092d10debd832
Tigo <rf.tigas@gmail.com>
2025-03-25 20:06:13 -0300

    config: add shadcn ui

--- client/src/pages/home.page..tsx
+++ client/src/pages/home.page..tsx
@@ -1,8 +1,11 @@
+import { Button } from "@/components/ui/button"
+
 function HomePage() {
   return (
     <section id="home">
-      <div className="flex flex-col justify-center h-screen items-center bg-zinc-800">
+      <div className="flex flex-col gap-4 justify-center h-screen items-center bg-zinc-800">
         <h1 className="text-zinc-50 text-4xl">Welcome to Snippout</h1>
+        <Button onClick={() => console.log("oi")}>Log in</Button>
       </div>
     </section>
   )
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,9 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
