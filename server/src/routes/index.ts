b2abbf1d4602f4f613b4fce5e42db6976a3b275f
tigo <rf.tigas@gmail.com>
2025-04-07 19:36:55 -0300

    feat(server): add category routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import userRouter from "./user.routes";
 import authRouter from "./auth.routes";
-
+import categoryRouter from "./category.routes";
 const router = Router();
 
 router.use("/user", userRouter);
 router.use("/auth", authRouter);
+router.use("/category", categoryRouter);
 
 export default router;
